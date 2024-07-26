04e43668e173347e60e3b72f2431c1a0e2f2d596
Will3m7 <ottvillem3000@gmail.com>
2024-07-26 10:05:11 +0300

    server test thing

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const fetch = require('node-fetch');
+const app = express();
+
+app.use(express.json());
+
+app.post('/api/webparser', async (req, res) => {
+  const { url } = req.body;
+  try {
+    const response = await fetch('https://uptime-mercury-api.azurewebsites.net/webparser', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ url })
+    });
+    const data = await response.json();
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+module.exports = app;
